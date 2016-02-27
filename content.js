788a8c9ebb2194e34ff4f1493fdbe00dd0977414
Anson Chung <anson.chung@gmail.com>
2016-02-27 00:55:37 -0500

    super hot

--- content.js
+++ content.js
@@ -1,14 +1,20 @@
-var showOverlay = function(){
-  var overlay = document.createElement('div');
-  overlay.id = "SUPERHOT-OVERLAY";
-  overlay.className = 'overlay';
-  overlay.innerHTML= "<div class=\'text\'><strong>SUPER</strong>HOT</div>";
+var isOverlayVisible = false;
 
-  document.body.appendChild(overlay);
-  setTimeout(removeOverlay, 20000);
+var showOverlay = function(){
+  if (!isOverlayVisible) {
+    isOverlayVisible = true;
+    var overlay = document.createElement('div');
+    overlay.id = "SUPERHOT-OVERLAY";
+    overlay.className = 'overlay';
+    overlay.innerHTML= "<div class=\'text\'><strong>SUPER</strong>HOT</div>";
+
+    document.body.appendChild(overlay);
+    setTimeout(removeOverlay, 20000);
+  }
 };
 
 var removeOverlay = function() {
   var overlay = document.getElementById("SUPERHOT-OVERLAY");
   document.body.removeChild(overlay);
+  isOverlayVisible = false;
 };
