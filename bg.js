7263b7a583db25294ddfc842d6407e02c8cf58cb
Anson Chung <anson.chung@gmail.com>
2016-02-27 15:52:44 -0500

    allowing for pause

--- bg.js
+++ bg.js
@@ -8,11 +8,18 @@ sound.type= 'audio/mpeg';
 sound.src= chrome.extension.getURL('assets/superhot_call.mp3');
 audioElement.appendChild(sound);
 
+var playing = false;
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-
-  chrome.tabs.executeScript(tab.id, {code: "showOverlay();"});
-
-  audioElement.load;
-  audioElement.play();
-
+  if (!playing) {
+    playing = true;
+    chrome.tabs.executeScript(tab.id, {code: "showOverlay();"});
+    audioElement.load;
+    audioElement.play();
+  } else {
+    playing = false;
+    chrome.tabs.executeScript(tab.id, {code: "hideOverlay();"});
+    audioElement.pause();
+    audioElement.currentTime = 0;
+  }
 });
--- content.js
+++ content.js
@@ -1,20 +1,19 @@
 var isOverlayVisible = false;
 
 var showOverlay = function(){
-  if (!isOverlayVisible) {
     isOverlayVisible = true;
     var overlay = document.createElement('div');
     overlay.id = "SUPERHOT-OVERLAY";
     overlay.className = 'super-hot-overlay';
     overlay.innerHTML= "<div class=\'super-hot-text\'><strong>SUPER</strong>HOT</div>";
-
     document.body.appendChild(overlay);
-    setTimeout(removeOverlay, 20000);
-  }
+
+    setTimeout(hideOverlay, 20000);
 };
 
-var removeOverlay = function() {
+var hideOverlay = function() {
   var overlay = document.getElementById("SUPERHOT-OVERLAY");
-  document.body.removeChild(overlay);
-  isOverlayVisible = false;
+  if (overlay != null) {
+    document.body.removeChild(overlay);
+  }
 };
